263d3cd6f4f2d8e58b35deabb3f29206fd4fcbd9
gul.sanober <gulsanobermemon@gmail.com>
2017-08-12 16:03:38 +0500

    branch created

---
+++ app/src/main/java/edu/aku/hassannaqvi/codi/activities/VaccineObservationActivity.java
@@ -0,0 +1,15 @@
+package edu.aku.hassannaqvi.codi.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import edu.aku.hassannaqvi.codi.R;
+
+public class VaccineObservationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vaccine_observation);
+    }
+}
